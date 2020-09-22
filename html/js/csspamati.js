22e403e70c2185cf3614b0c590c2a19d3de8c61c
Juris <61689342+jurisstrautmanis@users.noreply.github.com>
2020-09-22 13:19:28 +0300

    s
    sa

---
+++ html/js/csspamati.js
@@ -0,0 +1,3 @@
+var el = document.getElementById('heading1');
+el.style.color = 'green'
+console.log(el) 
---
+++ html/js/main.js
@@ -0,0 +1,2 @@
+   function hello(name) {
+    alert(`Hello ${name} !`); alert('Otra komanda!')}; 
